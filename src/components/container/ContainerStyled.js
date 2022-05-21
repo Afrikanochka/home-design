c05c187854522c35541dca3b3c8ed120dd54cd13
Afrikanochka <Mtgnatali@gmail.com>
2022-05-21 13:42:28 +0300

    fix

--- src/components/container/ContainerStyled.js
+++ src/components/container/ContainerStyled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ContainerStyled = styled.div`
-  padding-left: 25px;
+  /* padding-left: 25px; */
   padding-right: 25px;
   margin: 0 auto;
   max-width: 320px;
